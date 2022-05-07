2a519fae78b6ca410bf9979a7b21b2c88417acee
CESAR GUERRA <cesarricardo.guerra19@gmail.com>
2022-05-07 15:04:30 -0500

    ACTUALIZANDO!

--- buildAndRun.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-mvn clean package && docker build -t com.javabullets.javaee8/javaee8 .
-docker rm -f javaee8 || true && docker run -d -p 8080:8080 -p 4848:4848 --name employee-service com.javabullets.javaee8/javaee8 
