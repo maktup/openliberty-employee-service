7d0c4acd3b89b4525e67a52453d0dc3ec1fb5509
CESAR GUERRA <cesarricardo.guerra19@gmail.com>
2022-05-10 00:22:56 -0500

    ACTUALIZANDO!

--- src/main/java/pe/com/ibm/csm/service/imp/EmployeeServiceImp.java
+++ src/main/java/pe/com/ibm/csm/service/imp/EmployeeServiceImp.java
@@ -1,12 +1,5 @@
 package pe.com.ibm.csm.service.imp;
 
-import org.eclipse.microprofile.openapi.annotations.Operation;
-import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
-import org.eclipse.microprofile.openapi.annotations.media.Content;
-import org.eclipse.microprofile.openapi.annotations.media.Schema;
-import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
-import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
-import org.eclipse.microprofile.openapi.annotations.responses.APIResponseSchema;
 import java.util.ArrayList;
 import java.util.List;
 import javax.ws.rs.GET;
@@ -14,7 +7,11 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response; 
+import javax.ws.rs.core.Response;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponseSchema;
 import pe.com.ibm.csm.bean.Employee;
 import pe.com.ibm.csm.bean.ListEmployee;
 import pe.com.ibm.csm.service.EmployeeService;
